5e6e821f7a9a40e900a6a5e9d533ac9912333f91
igor-romasiuk <igor.romasiuk@gmail.com>
2025-12-04 19:36:53 +0200

    feat: start work on city searching logic

--- weather-dashboard/src/App.tsx
+++ weather-dashboard/src/App.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import './App.css'
 import CityCard from './components/CityCard'
+import { useQuery } from '@tanstack/react-query'
 
 const cities = [
   { name: 'Kyiv', latitude: 50.45, longitude: 30.52 },
@@ -8,9 +10,36 @@ const cities = [
 ]
 
 function App() {
+  const [value, setValue] = useState('')
+
+  const fetchSearchCity = async () => {
+    try {
+      const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(value.trim().toLowerCase())}&count=5&language=uk`)
+
+      const data = await response.json()
+
+      return data
+    } catch (error) {
+      if (error instanceof Error) throw error
+        throw new Error('Request failed')
+    }
+  }
+
+  const {data, isLoading} = useQuery({
+    queryKey: ['city-search', value.trim().toLowerCase()],
+    queryFn: () => fetchSearchCity(),
+    enabled: value.length >= 2,
+  })
+
   return (
     <div>
       <h1>Weather Dashboard</h1>
+
+      <input type="text" value={value} onChange={e => setValue(e.target.value)}/>
+      <ul>
+        <li></li>
+      </ul>
+
       <div>
         {cities.map(city => (
           <CityCard key={city.name} {...city} />
